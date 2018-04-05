a0160803faa402fd731757d7ce35afc1397931b7
Nick Stanish <nickstanish@gmail.com>
2018-04-04 21:06:24 -0500

    Update dev dependencies

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,26 +1,23 @@
 const path = require('path');
 const webpack = require('webpack');
-const { LoaderOptionsPlugin } = webpack;
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = function (env = 'development') {
+module.exports = function buildWebpackConfig(env = 'development') {
   const config = {
     context: __dirname,
     devtool: env === 'production' ? 'source-map' : '#inline-source-map',
     entry: [
-      './index.js',
+      './index.js'
     ],
     output: {
       path: path.resolve(__dirname, 'build'),
       publicPath: '/build',
-      filename: 'bundle.js',
+      filename: 'bundle.js'
     },
     resolve: {
       alias: {
         reprogressbars: path.join(__dirname, '..', 'src/index.js'),
-      },
+      }
     },
-
     module: {
       rules: [{
         test: /\.js$/,
@@ -37,37 +34,12 @@ module.exports = function (env = 'development') {
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify(env),
       })
-    ]
+    ],
+    devServer: {
+      contentBase: path.resolve(__dirname),
+      port: process.env.PORT || '3000'
+    }
   };
-
-  if (env === 'production') {
-    config.plugins = [
-      ...config.plugins,
-      new LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      }),
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          compress: {
-            conditionals: true,
-            unused: true,
-            comparisons: true,
-            sequences: true,
-            dead_code: true,
-            evaluate: true,
-            if_return: true,
-            join_vars: true
-          },
-          output: {
-            comments: false
-          },
-          sourceMap: true,
-        }
-      }),
-      new webpack.HashedModuleIdsPlugin()
-    ];
-  }
 
   return config;
 };
